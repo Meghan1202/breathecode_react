4da4f453f94f77f4b66a0a41a8485b823455c11b
Meghana Khuntia <meghanakhuntia12@gmail.com>
2021-01-04 09:46:13 +0530

    init-2

---
+++ exercises/05.1-second-styles/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
+
+const buttonStyles = {
+	//write button styles here
+    background: "yellow",
+    border: "none",
+    color: "black",
+};
+
+const badgeStyles = {
+    background: "red",
+    color: "white",
+	//write the span styles here
+
+};
+
+const Badge = props => {
+	return (
+		<button style={buttonStyles} type="button" className="btn btn-primary">
+			{props.label}
+			<span style={badgeStyles} className="badge badge-light">
+				{props.number}
+			</span>
+		</button>
+	);
+};
+Badge.propTypes = {
+	label: PropTypes.string,
+	number: PropTypes.string
+};
+//dont forget to change the label
+ReactDOM.render(<Badge label="Alerts" number="2" />, document.querySelector("#myDiv"));
+
